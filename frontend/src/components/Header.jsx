26888a6bc1a6ad5145f91f1aedfc0ed849d779b7
Abhinav Bangar <a.banger2005@gmail.com>
2026-03-09 18:32:52 +0530

    header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css';
+
+function Header(){
+    return(
+        <>
+            <div className='header-container'>
+                <button className='site-name'>Stride</button>
+                
+            </div>
+        </>
+    );
+}
+
+export default Header;
